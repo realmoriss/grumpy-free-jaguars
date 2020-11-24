717db0f8c23d3d3eb0492a3074eaf7b6eceb986b
Csaba Sarkadi <61619504+enoperm@users.noreply.github.com>
2020-11-24 21:11:50 +0100

    server: run gofmt

--- server/server.go
+++ server/server.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-    "github.com/gin-gonic/nosurf"
-    "github.com/gwatts/gin-adapter"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/nosurf"
+	"github.com/gwatts/gin-adapter"
 
-    user "server/endpoint/user"
+	user "server/endpoint/user"
 )
 
 func main() {
-    // TODO: Setup db.
-    db := struct{}{}
+	// TODO: Setup db.
+	db := struct{}{}
 
 	router := gin.Default()
 	router.Use(adapter.Wrap(nosurf.NewPure))
 
-    router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/*")
 
-    {
-        g := router.Group("/user")
-        user.NewEndpoint(g, db)
-    }
-    http.ListenAndServe(":3000", nosurf.New(router))
+	{
+		g := router.Group("/user")
+		user.NewEndpoint(g, db)
+	}
+	http.ListenAndServe(":3000", nosurf.New(router))
 	router.Run(":3000")
 }
